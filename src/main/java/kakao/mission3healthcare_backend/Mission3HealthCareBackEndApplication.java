deb91e59afaf673df4ae1e0e7200493a32dfbc13
박지혁 <pjh5365@naver.com>
2024-07-04 15:59:40 +0900

    Init: 개발에 필요한 초기 프로젝트 생성 (#2)
    추가한 의존성
    - Spring Web
    - Spring Security
    - Spring Data JPA
    - H2 Database
    - MariaDB Driver
    - Lombok
    
    Related to: #1

---
+++ src/main/java/kakao/mission3healthcare_backend/Mission3HealthCareBackEndApplication.java
@@ -0,0 +1,13 @@
+package kakao.mission3healthcare_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mission3HealthCareBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mission3HealthCareBackEndApplication.class, args);
+	}
+
+}
