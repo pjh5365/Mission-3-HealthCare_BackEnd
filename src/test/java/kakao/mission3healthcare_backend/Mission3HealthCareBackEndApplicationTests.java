14dbb2eeecf60011bf806710cbf8dbecd6cdac40
pjh5365 <pjh5365@naver.com>
2024-07-04 15:51:41 +0900

    Init: 개발에 필요한 초기 프로젝트 생성
    추가한 의존성
    - Spring Web
    - Spring Security
    - Spring Data JPA
    - H2 Database
    - MariaDB Driver
    - Lombok
    
    Related to: #1

---
+++ src/test/java/kakao/mission3healthcare_backend/Mission3HealthCareBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package kakao.mission3healthcare_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mission3HealthCareBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
