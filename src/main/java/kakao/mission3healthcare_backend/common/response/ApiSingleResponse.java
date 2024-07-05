149424372426b335adaf44df019f2c91fd9faf33
pjh5365 <pjh5365@naver.com>
2024-07-05 22:02:52 +0900

    Feat: API처리 결과를 단일객체 형태로 담아 클라이언트로 전송할 공통 객체 작성
    Related to: #3

---
+++ src/main/java/kakao/mission3healthcare_backend/common/response/ApiSingleResponse.java
@@ -0,0 +1,19 @@
+package kakao.mission3healthcare_backend.common.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+/**
+ * 서비스가 반환하는 단일 객체를 담아 클라이언트에 보내기 위한 객체
+ *
+ * @author : parkjihyeok
+ * @since : 2024/07/05
+ */
+@Getter
+@AllArgsConstructor
+public class ApiSingleResponse<T> {
+
+	private String status; // 응답 코드에 대한 정보
+	private String message; // 응답 메시지에 대한 정보
+	private T data; // 응답객체를 공통으로 사용하기 위해 제네릭으로 받아 처리하기
+}
