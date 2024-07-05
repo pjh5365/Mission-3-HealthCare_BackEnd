8a053563577b33231f2e5769a584858eb33cefcd
pjh5365 <pjh5365@naver.com>
2024-07-05 22:11:13 +0900

    Feat: 개발단계에서 h2-console을 사용하기 위해 시큐리티 설정파일에 해당 설정 추가
    Related to: #3

--- src/main/java/kakao/mission3healthcare_backend/config/SecurityConfig.java
+++ src/main/java/kakao/mission3healthcare_backend/config/SecurityConfig.java
@@ -1,10 +1,12 @@
 package kakao.mission3healthcare_backend.config;
 
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -24,12 +26,16 @@ public class SecurityConfig {
 				// 로그인, 로그아웃, 회원가입은 누구나 접근가능
 				auth.requestMatchers("/api/login", "/api/logout", "/api/users")
 						.permitAll()
+						.requestMatchers(PathRequest.toH2Console()).permitAll() // 개발단계에선 h2-console 허용
 						.anyRequest()
 						.authenticated()
 		);
 
 		// csrf는 사용하지 않음
 		http.csrf(AbstractHttpConfigurer::disable);
+
+		http.headers(headers -> headers
+				.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin)); // h2-console 허용
 
 		return http.build();
 	}
