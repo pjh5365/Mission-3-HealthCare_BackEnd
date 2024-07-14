47774c765934ba7487d29be56df6abccf714432a
pjh5365 <pjh5365@naver.com>
2024-07-14 16:11:41 +0900

    Refactor: 식단서비스 로직 수정 및 추가
    Related to: #9

--- src/main/java/kakao/mission3healthcare_backend/Diet/service/DietService.java
+++ src/main/java/kakao/mission3healthcare_backend/Diet/service/DietService.java
@@ -10,10 +10,15 @@ import kakao.mission3healthcare_backend.common.service.CommonService;
 import kakao.mission3healthcare_backend.diet.domain.dto.request.DietDeleteRequest;
 import kakao.mission3healthcare_backend.diet.domain.dto.request.DietRequest;
 import kakao.mission3healthcare_backend.diet.domain.dto.request.DietUpdateRequest;
+import kakao.mission3healthcare_backend.diet.domain.dto.response.DietResponse;
+import kakao.mission3healthcare_backend.diet.domain.dto.response.FoodInfo;
+import kakao.mission3healthcare_backend.diet.domain.dto.response.NutrientResponse;
 import kakao.mission3healthcare_backend.diet.domain.entity.Diet;
 import kakao.mission3healthcare_backend.diet.domain.entity.Food;
+import kakao.mission3healthcare_backend.diet.domain.entity.Nutrient;
 import kakao.mission3healthcare_backend.diet.repository.DietRepository;
 import kakao.mission3healthcare_backend.diet.repository.FoodRepository;
+import kakao.mission3healthcare_backend.diet.repository.NutrientRepository;
 import lombok.RequiredArgsConstructor;
 
 /**
@@ -29,6 +34,35 @@ public class DietService {
 	private final CommonService commonService;
 	private final DietRepository dietRepository;
 	private final FoodRepository foodRepository;
+	private final NutrientRepository nutrientRepository;
+
+	/**
+	 * 한 사용자의 모든 식단 정보를 담아 반환하는 메서드
+	 *
+	 * @param username 회원ID
+	 * @return 회원이 기록한 모든 식단정보
+	 */
+	public List<DietResponse> getDiets(String username) {
+		return dietRepository.findByUsername(username)
+				.stream().map(this::getDiet)
+				.toList();
+	}
+
+	/**
+	 * 한 식단에 대한 종합정보를 담아 반환하는 메서드
+	 *
+	 * @param diet 식단정보
+	 * @return 식단에 대한 종합정보
+	 */
+	public DietResponse getDiet(Diet diet) {
+		List<FoodInfo> foodInfos = foodRepository.findByDietId(diet.getId())
+				.stream()
+				.map(Food::getFoodName) // 음식 이름들을 가져와서
+				.map(this::getFoodInfo) // 각 음식에 대한 정보를 가져온다.
+				.toList();
+
+		return new DietResponse(diet.getMember().getUsername(), diet.getMealType(), foodInfos, diet.getDietDate());
+	}
 
 	/**
 	 * 식단을 추가하는 메서드
@@ -42,6 +76,7 @@ public class DietService {
 		Diet diet = Diet.builder()
 				.member(member)
 				.mealType(dietRequest.getMealType())
+				.dietDate(dietRequest.getDietDate())
 				.build();
 
 		dietRepository.save(diet);
@@ -100,5 +135,31 @@ public class DietService {
 	private Diet findDiet(Long id) {
 		return dietRepository.findById(id)
 				.orElseThrow(() -> new IllegalArgumentException("해당 식단 정보를 찾을 수 없습니다."));
+	}
+
+	/**
+	 * 음식에 대한 세부정보를 반환하는 메서드
+	 *
+	 * @param foodName 음식이름
+	 * @return 음식에 대한 세부정보를 담고있는 객체
+	 */
+	private FoodInfo getFoodInfo(String foodName) {
+		List<NutrientResponse> nutrients = nutrientRepository
+				.findByFoodName(foodName) // 음식이름으로 영양소 정보들을 가져온다.
+				.stream()
+				.map(this::nutrientToDto) // 영양소 정보를 응답하기 위한 객체로 변경한다.
+				.toList();
+
+		return new FoodInfo(foodName, nutrients);
+	}
+
+	/**
+	 * 영양소 엔티티를 DTO로 변경해주는 메서드
+	 *
+	 * @param nutrient 영양소 엔티티
+	 * @return 반환할 DTO
+	 */
+	private NutrientResponse nutrientToDto(Nutrient nutrient) {
+		return new NutrientResponse(nutrient.getNutrientType(), nutrient.getAmount());
 	}
 }
